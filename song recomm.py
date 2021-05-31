0b723748a6b971603fc676deefb8df1d818765f4
Thomaspverghese <85082434+Thomaspverghese@users.noreply.github.com>
2021-05-31 11:37:17 +0530

    Create song recomm.py
    this is a testing phase only on a python program

---
+++ song recomm.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 15:54:24 2021
+
+@author: 91860
+"""
+def songrec():
+    team_list={"melody":"mandara chepu undo","fast":"aakh maare"}
+    mood=print("which mood are you in? melody or fast")
+    mood=input()
+    if (mood in team_list)==True:
+        print(team_list[mood])
+    else:
+        print("sorry song not available")
